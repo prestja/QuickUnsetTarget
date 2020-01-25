36a04dd460207e15b8661eb2a32b4a8a202bf311
Jacob Preston <prestja@gmail.com>
2020-01-24 20:32:17 -0600

    Initial commit

---
+++ Quick Unset Target/QuickUnsetTarget.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using KSP.UI.Screens;
+
+namespace prestja {
+	[KSPAddon(KSPAddon.Startup.Flight, false)]
+	public class QuickUnset : MonoBehaviour {
+		private ApplicationLauncherButton launcherButton;
+
+		private void Start () {
+			launcherButton = ApplicationLauncher.Instance.AddModApplication(
+				() => { FlightGlobals.fetch.SetVesselTarget(null);},
+				null, null, null, null, null, 
+				ApplicationLauncher.AppScenes.FLIGHT, 
+				GameDatabase.Instance.GetTexture("QuickUnsetTarget/icon", false));
+		}
+
+		private void OnDestroy() {
+			ApplicationLauncher.Instance.RemoveModApplication(launcherButton);
+		}
+	}
+}
